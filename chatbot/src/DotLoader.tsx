80cb219e79f885f818a719d2eab4c10df5ffd106
bertrand taillandier <taillandierbertrand@gmail.com>
2023-11-15 15:47:54 +0100

    feat(style): make it more modern

--- chatbot/src/DotLoader.tsx
+++ chatbot/src/DotLoader.tsx
@@ -1,37 +1,40 @@
 import styled from 'styled-components';
 
+import {colors} from './colors';
+
 const Dots = styled.div`
   position: relative;
-  left: -9999px;
-  width: 10px;
-  height: 10px;
+  left: -9994px;
+  top: 6px;
+  width: 4px;
+  height: 4px;
   border-radius: 5px;
-  background-color: black;
-  color: black;
+  background-color: ${colors.text};
+  color: ${colors.text};
   animation: dot-loader 1.3s infinite linear;
   margin: 0 12px;
 
   @keyframes dot-loader {
   0% {
-    box-shadow: 9984px 0 0 0 black, 9999px 0 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
   16.667% {
-    box-shadow: 9984px -10px 0 0 black, 9999px 0 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px -10px 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
   33.333% {
-    box-shadow: 9984px 0 0 0 black, 9999px 0 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
   50% {
-    box-shadow: 9984px 0 0 0 black, 9999px -10px 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px -10px 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
   66.667% {
-    box-shadow: 9984px 0 0 0 black, 9999px 0 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
   83.333% {
-    box-shadow: 9984px 0 0 0 black, 9999px 0 0 0 black, 10014px -10px 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px -10px 0 0 ${colors.text};
   }
   100% {
-    box-shadow: 9984px 0 0 0 black, 9999px 0 0 0 black, 10014px 0 0 0 black;
+    box-shadow: 9984px 0 0 0 ${colors.text}, 9999px 0 0 0 ${colors.text}, 10014px 0 0 0 ${colors.text};
   }
 }
 `;
---
+++ chatbot/src/colors.tsx
@@ -0,0 +1,8 @@
+export const colors = {
+	bg: '#2A2A2C',
+	bg_dark: '#1E1E1F',
+	bg_light: '#414141',
+	text: '#ECECEC',
+	text_neutral:'#AFB0B2',
+	highlight: '#593CFB',
+};
